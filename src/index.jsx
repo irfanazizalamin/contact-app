64c9a3bc80ceeb2cb98db9e9959c22b1341e28d9
irfanazizalamin <irfanaziz2929@gmail.com>
2024-05-31 20:39:29 +0700

    split add page

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,12 @@
 import { createRoot } from "react-dom/client";
-import ContactApp from "./ContactApp";
+import ContactApp from "./components/ContactApp";
+import { BrowserRouter } from "react-router-dom";
 // import FormApp from "./FormApp";
 
 const root = createRoot(document.getElementById("root"));
-root.render(<ContactApp />)
+root.render(
+    <BrowserRouter>
+        <ContactApp />
+    </BrowserRouter>
+)
 // root.render(<FormApp />)
